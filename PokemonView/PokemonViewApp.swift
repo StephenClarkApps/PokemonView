421173a95845d2c0c8d5ce5128e7215d987a60f0
StephenClarkApps <contact.stephenclark@gmail.com>
2024-04-22 14:34:58 +0100

    Initial Commit

---
+++ PokemonView/PokemonViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonViewApp.swift
+//  PokemonView
+//
+//  Created by Stephen Clark on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
