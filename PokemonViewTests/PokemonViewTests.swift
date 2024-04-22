4b2c0e399dc545469f3d0fc06d51def715c601f6
StephenClarkApps <contact.stephenclark@gmail.com>
2024-04-22 14:44:08 +0100

    Started sketching out unit tests. Created and added a logo for the app to the asset catalog.

--- PokemonViewTests/PokemonViewTests.swift
+++ PokemonViewTests/PokemonViewTests.swift
@@ -5,6 +5,7 @@
 //  Created by Stephen Clark on 22/04/2024.
 //
 
+@testable import PokemonView
 import XCTest
 
 final class PokemonViewTests: XCTestCase {
@@ -17,19 +18,12 @@ final class PokemonViewTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // Test 1: Fetch Pokémon List
+    
+    
+    // Test 2: Search Pokémon List
+    
+    
+    // Test 3: Fetch details for a particular Pokémon
+    
 }
