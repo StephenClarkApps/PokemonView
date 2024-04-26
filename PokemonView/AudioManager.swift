7807d9eb5681e6bca3402f52a08febdc35813f60
StephenClarkApps <contact.stephenclark@gmail.com>
2024-04-26 17:29:35 +0100

    Modify the audio manager such that if we've already dowloaded the ogg file for a particular Pokemon then we don't do so again. Prefer the non-legacy cry.

--- PokemonView/AudioManager.swift
+++ PokemonView/AudioManager.swift
@@ -19,34 +19,50 @@ class AudioManager {
         FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
     }
 
+    
+    /// Play the Pokemon cry, this version prefers the latest version of the cry over legacy (TODO: - make this a setting for the user)
+    /// - Parameters:
+    ///   - legacyUrl: URL associated with the ogg file with the legacy cry of the Pokemon
+    ///   - latestUrl: URL associated with the ogg file with the latest version of the cry of the Pokemon
     func playPokemonCry(legacyUrl: String?, latestUrl: String?) {
-        guard let urlString = legacyUrl ?? latestUrl, let url = URL(string: urlString) else {
+        guard let urlString = latestUrl ?? legacyUrl, let url = URL(string: urlString) else {
             print("No valid URL for the cry sound.")
             return
         }
         downloadAndPlay(url: url)
     }
 
+    
+    /// Function to dowload and play the ogg file which are the Pokémon's cries
+    /// - Parameter url: URL to use
     private func downloadAndPlay(url: URL) {
         let localUrl = documentsDirectory.appendingPathComponent(url.lastPathComponent)
-        let task = URLSession.shared.downloadTask(with: url) { tempLocalUrl, response, error in
-            guard let tempLocalUrl = tempLocalUrl, error == nil else {
-                print("Failed to download file: \(error?.localizedDescription ?? "No error information.")")
-                return
-            }
-            do {
-                if FileManager.default.fileExists(atPath: localUrl.path) {
-                    try FileManager.default.removeItem(at: localUrl)
+
+        // Check if the file already exists locally
+        if FileManager.default.fileExists(atPath: localUrl.path) {
+            // If file exists, directly decode and play it (Avoid downloading the same file twice)
+            self.decodeOGGFile(at: localUrl)
+        } else {
+            // If file does not exist, download it
+            let task = URLSession.shared.downloadTask(with: url) { tempLocalUrl, response, error in
+                guard let tempLocalUrl = tempLocalUrl, error == nil else {
+                    print("Failed to download file: \(error?.localizedDescription ?? "No error information.")")
+                    return
                 }
-                try FileManager.default.moveItem(at: tempLocalUrl, to: localUrl)
-                self.decodeOGGFile(at: localUrl)
-            } catch {
-                print("File handling error: \(error)")
+                do {
+                    // Move downloaded file to the permanent location
+                    try FileManager.default.moveItem(at: tempLocalUrl, to: localUrl)
+                    self.decodeOGGFile(at: localUrl)
+                } catch {
+                    print("File handling error: \(error)")
+                }
             }
+            task.resume()
         }
-        task.resume()
     }
 
+    /// Use the OggDecoder dependency to covert to a format we can play on iOS
+    /// - Parameter localUrl: the local URL where the file to be decoded is currently stored
     private func decodeOGGFile(at localUrl: URL) {
         let decoder = OGGDecoder()
         decoder.decode(localUrl) { savedWavUrl in
@@ -58,6 +74,9 @@ class AudioManager {
         }
     }
 
+    
+    /// Setup the audio playback session, using shared instance, and set playback mode etc
+    /// - Parameter url: local URL of decoded audio file for playback
     private func setupPlayback(with url: URL) {
         let audioSession = AVAudioSession.sharedInstance()
         do {
